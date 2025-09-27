4b7d44296ff469d9298ebdacddf18135946fba81
Kevin Rogers <kevin@kevinrogers.org>
2025-09-27 21:23:25 +0000

    Initial changes for QR code generation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QrCodeComponent } from './qr-code/qr-code.component';
+import { JumboBannerComponent } from './jumbo-banner/jumbo-banner.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'qr', component: QrCodeComponent },
+  { path: '**', component: JumboBannerComponent }, // Default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

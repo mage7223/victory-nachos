4784e83fbdf3f7119b9a8bd50e18bab3b845f57e
Kevin Rogers <krogers@ainsight.com>
2025-09-27 23:58:13 -0400

    Added autoselect directive but it may not work in this sutuation

---
+++ src/app/directives/autoselect.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AutoselectDirective } from './autoselect.directive';
+
+describe('AutoselectDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AutoselectDirective();
+    expect(directive).toBeTruthy();
+  });
+});
